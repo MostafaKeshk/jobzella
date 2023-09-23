5a345c20de0eb95b2c0cce5bcb117494b252235c
mostafa <mostafa_mohamed12326@yahoo.com>
2023-09-23 18:59:06 +0300

    update env

--- frontend/config/url.ts
+++ frontend/config/url.ts
@@ -1,4 +1,4 @@
 // export const url = "http://localhost:5000";
 // export const url = "https://jobzella-server.vercel.app";
 
-export const url = process.env.SERVER_URL;
+export const url = process.env.NEXT_PUBLIC_SERVER_URL;
